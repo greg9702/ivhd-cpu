c93f9f7a21c4ce79d6ebaa79f9348c2eaf7734a0
Dawid Witek <dwitekw@gmail.com>
2020-06-27 14:05:15 +0200

    Final results added

--- src/caster/constants.h
+++ src/caster/constants.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#define momentum_rate 0.9
+
+
 // RCV
 // iterations 5000, neighbours 3, random_neighbours 1
-//#define w_random 0.1
-//#define learning_rate 0.01
-#define momentum_rate 0.9
+#define w_random 0.1
+#define learning_rate 0.02
 
 // MNIST
 // iterations 5000, neighbours 2, random_neighbours 1
-#define w_random 0.01
-#define learning_rate 0.01
+//#define w_random 0.01
+//#define learning_rate 0.01
